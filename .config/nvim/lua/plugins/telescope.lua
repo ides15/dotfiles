81acbbecb3ff6e553029653561d72f15f812c7f0
John Ide <ide.johnc@gmail.com>
2024-05-14 13:34:59 -0600

    live grep through entire directory

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,6 @@ return {
   "nvim-telescope/telescope.nvim",
   keys = {
     { "<leader><space>", LazyVim.telescope("files", { cwd = false }), desc = "Find Files (Root Dir)" },
-    { "<leader>/", LazyVim.telescope("live_grep"), desc = "Grep (Root Dir)" },
+    { "<leader>/", LazyVim.telescope("live_grep", { cwd = false }), desc = "Grep (Root Dir)" },
   },
 }
